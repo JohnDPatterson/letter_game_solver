18be4790c0ccf80da1f14d770b307df3b034ec07
JohnDPatterson <johndavidpatterson93@gmail.com>
2023-07-17 07:48:39 -0700

    Let's solve the spelling bee!

---
+++ solve_letters.py
@@ -0,0 +1,68 @@
+import sys
+import getopt
+from nltk.corpus import words
+from datetime import datetime
+import os
+import string
+from sys import exit
+
+
+
+def main(args):
+    opts, args = getopt.getopt(args, 'l:c:h', ['letters=', 'central=', 'help'])
+	
+    for opt, arg in opts:
+        if opt in ('-h', '--help'):
+            print('option -l,--letters= not central letters: option -c,--central= central letter')
+            exit()
+        if opt in ('-l', '--letters'):
+            l=arg
+        if opt in ('-c','--central'):
+            c=arg
+        
+    if 'c' not in locals() or 'l' not in locals():
+        print('please enter both central letter and other letters')
+        exit()
+    
+    print('central letter is: ' + c)
+    print('other letters are: ' + l)
+
+    lowers=list(string.ascii_lowercase)    
+
+    l_list=list(l)
+    
+    other_letters=[]
+    for j in lowers:
+        if j not in l_list and j!=c:
+            other_letters.append(j)
+    
+    fname='words_'+ datetime.today().strftime('%Y%m%d')+'.txt' 
+    
+    word_list = words.words()
+    count=0
+    out_list=[]
+    for j in word_list:
+       
+        w=list(str.lower(j))
+        
+        count+=1
+        
+        if c not in w:
+            continue
+        elif len(w)<4:
+            continue
+        elif len(set(w).intersection(other_letters)) > 0:
+            continue
+        
+        out_list.append(j+'\n')
+    
+    with open(fname, 'w') as f:
+        for j in out_list:
+            f.write(j)
+        
+    
+if __name__ == '__main__':
+    main(sys.argv[1:]) #Send list of arguments
+
+#main(['-c','e','-l','stpr'])    
+
